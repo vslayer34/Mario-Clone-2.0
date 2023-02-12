5cc8e246677c22bc404820f10e9da9ff75aa7b72
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-02-12 07:54:16 +0200

    added the player controller
     - horizontal movement
     - sprite flipping

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Stats playerStats;
+
+    Rigidbody2D playerRB;                                   // rigid body variabe
+
+    [SerializeField]
+    SpriteRenderer playerSprite;                            // reference to the sprite renderer
+
+    [SerializeField]
+    Joystick joystick;                                      // reference to the joystick
+
+    float speed;                                            // speed of the player
+
+    float movementX;                                        // variable to save the joystick horizantal movement
+
+    void Start()
+    {
+        // get a referance to the player rigid body
+        playerRB = GetComponent<Rigidbody2D>();
+        
+        // set the player speed to value in the player stats
+        speed = playerStats.speed;
+    }
+
+    void Update()
+    {
+        // update the values of joysticj horziontal movement
+        movementX = joystick.Horizontal;
+    }
+
+    void FixedUpdate()
+    {
+        Move(movementX);
+    }
+
+    /// <summary>
+    /// get the input of the joystick and moves the player rigidbody accordingly
+    /// flip the sprite renderer to the corret direction
+    /// </summary>
+    /// <param name="input"></param>
+    void Move(float input)
+    {
+        // flip the sprite renderer
+        if (input < 0)
+            playerSprite.flipX = true;
+        else
+            playerSprite.flipX = false;
+
+        // move the player
+        playerRB.velocity = new Vector2(input * Time.deltaTime * speed, playerRB.velocity.y);
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/Stats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Stats", menuName = "stats")]
+public class Stats : ScriptableObject
+{
+    public float speed;
+}
