f9a2fb2671dc79598404939412241a2c52eee95a
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-02-12 09:47:33 +0200

    added jumping still need bit of modifying and balance

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,47 +15,65 @@ public class PlayerController : MonoBehaviour
     Animator playerAnimator;
 
     float speed;                                            // speed of the player
+    float jumpForce;
 
-    float movementX;                                        // variable to save the joystick horizantal movement
+    // variables to save the joystick input movement
+    float movementX;
+    float movementY;
 
     void Start()
     {
         // get a referance to the player rigid body
         playerRB = GetComponent<Rigidbody2D>();
         
-        // set the player speed to value in the player stats
-        speed = playerStats.speed;
+        
     }
 
     void Update()
     {
-        // update the values of joysticj horziontal movement
+        // set the player speed to value in the player stats
+        speed = playerStats.speed;
+        jumpForce = playerStats.jumpForce;
+
+        // update the values of joystick movement
         movementX = joystick.Horizontal;
+        movementY = joystick.Vertical;
     }
 
     void FixedUpdate()
     {
         Move(movementX);
+        Jump(movementY);
     }
 
     /// <summary>
     /// get the input of the joystick and moves the player rigidbody accordingly
     /// flip the sprite renderer to the corret direction
     /// </summary>
-    /// <param name="input"></param>
-    void Move(float input)
+    /// <param name="inputX"></param>
+    void Move(float inputX)
     {
-        if (input == 0)
+        if (inputX == 0)
             playerRB.velocity = new Vector2(0.0f, playerRB.velocity.y);           // instant stop
 
         // flip the sprite renderer
-        else if (input < 0)
+        else if (inputX < 0)
             playerSprite.flipX = true;
         else
             playerSprite.flipX = false;
 
         // move the player
-        playerRB.velocity = new Vector2(input * Time.deltaTime * speed, playerRB.velocity.y);
-        playerAnimator.SetFloat(AnimatorParam.playerSpeed, Mathf.Abs(input));
+        playerRB.velocity = new Vector2(inputX * Time.deltaTime * speed, playerRB.velocity.y);
+        playerAnimator.SetFloat(AnimatorParam.playerSpeed, Mathf.Abs(inputX));
+    }
+
+
+    void Jump(float inputY)
+    {
+        if (inputY > 0.5)
+        {
+            Vector2 jumpDirection = Vector2.up * jumpForce * Time.deltaTime;
+            playerRB.AddForce(jumpDirection, ForceMode2D.Impulse);
+        }
     }
 }
--- Assets/Scripts/Scriptable Objects/Stats.cs
+++ Assets/Scripts/Scriptable Objects/Stats.cs
@@ -6,4 +6,5 @@ using UnityEngine;
 public class Stats : ScriptableObject
 {
     public float speed;
+    public float jumpForce;
 }
